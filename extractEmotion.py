08fadfa0ca2d70743df01c1f0b25ec71615c53f6
AlanConstantine <rlalan@outlook.com>
2020-03-17 00:19:56 +0800

    Update

--- extractEmotion.py
+++ extractEmotion.py
@@ -23,12 +23,21 @@ from sklearn.utils import shuffle
 import json
 
 import paralleldots
-paralleldots.set_api_key("OwoCuujTTYS5o3Vrx0M4A7RevaHLs7uUHlUD3Afa0XI")
+paralleldots.set_api_key("OwoCuujTTYS5o3Vrx0M4A7RevaHLs7uUHlUD3Afa0XI") # laualan@hku.hk
+
+keys = [
+#         'OwoCuujTTYS5o3Vrx0M4A7RevaHLs7uUHlUD3Afa0XI', # laualan@hku.hk
+        'DwGe3alGduDGJnE1FT3O112wMDNJhZDnGysR0KFfZgY', # rlalan@outlook.com
+        'JIkATWugt8LIP3PbQrdm6cfm1hZ4DbouVoyw8oAQqhI', # 394414515@qq.com
+        'Vf9mBAl7wp0s8Fl43E9aDtVPOZhftIAv0pYJvIlYGD4', # 806124854@qq.com
+        '8gAwuDOEE92zakGfDXc6PoCtqhVop0htEnEd4IHFe0U', # yleun.lau@gmail.com
+        'xTNXoCxtbg36jCgT6ArKgyOPJtnJdf5QQpEEKYoSwu8' # alanconstantinelau@gmail.com
+        ]
 
 
 # %%
-df = pd.read_csv('./en_df_sample_30.csv')
-df = df.sample(frac=1)
+df = pd.read_csv('./reindex_df.csv')
+# df = df.sample(frac=1)
 print(df.shape)
 
 
@@ -64,6 +73,17 @@ def update_emo(index, emo):
 def update_error(index):
     with open(r'./error', 'a') as fn:
         fn.write(str(index) + '\n')
+        
+def get_stopindex():
+#     with open(r'./error', 'r') as fn:
+#         index = (fn.readlines()[-1]).split('\t')[0]
+    try:
+        with open(r'./lyrics_emotion.json', 'r') as fn:
+                emo_dict = json.load(fn)
+        return max(list(map(lambda e: int(e), list(emo_dict.keys()))))
+    except Exception as e:
+        return -1
+    
 
 
 # %%
@@ -73,19 +93,36 @@ index_list = df.index.tolist()
 total = len(index_list)
 count = 1
 error_count = 0
+key_count = 0
 
-for index, lyrics in zip(index_list, lyric_emo):
+for index, lyrics in list(zip(index_list, lyric_emo))[get_stopindex() + 1:]:
     randtime = randint(0, 4)
     try:
         time.sleep(randtime)
         response = paralleldots.emotion(lyrics)
+        if 'code' in response and key_count < len(keys):
+            # if response['code'] == 403:
+            key_count += 1
+            print('Current key:', keys[key_count])
+            paralleldots.set_api_key(keys[key_count])
+            response = paralleldots.emotion(lyrics)
+        elif key_count == len(keys):
+            print('Stop at', str(index))
+            update_error(str(index) + '\tStop.')
+            break
+        if 'message' in response:
+            print('Stop at', str(index))
+            update_error(str(index) + '\t' + str(response['message']))
+            break
         finished_index = update_emo(index, response)
+#         TODO
         print(finished_index, 'done: ', str(round((count/total)*100, 5)))
         count += 1
     except Exception as e:
         update_error(str(index) + '\t' + str(e))
         error_count += 1
         print(e)
+        pass
 
 print(error_count)
 
