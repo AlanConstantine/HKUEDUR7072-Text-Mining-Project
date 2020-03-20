ac2a4a0fb2eb367ec7ab71ef95be2ef664059c18
LIU Ruilun <rlalan@outlook.com>
2020-03-21 01:11:26 +0800

    Update

--- training.py
+++ training.py
@@ -22,6 +22,7 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
 
 
 from time import time
@@ -158,6 +159,7 @@ scoring = kappa_scorer
 
 # %%
 def svm_model(xtrain, xtest, ytrain, ytest, batch):
+    t0 = time()
     param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
                   'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], 
                  'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}
@@ -167,7 +169,7 @@ def svm_model(xtrain, xtest, ytrain, ytest, batch):
     searcher = clf.fit(xtrain, ytrain)
     estimator = searcher.best_estimator_
     f1, kappa = measures(estimator.predict(xtest), ytest)
-    print('[SVM] training: ', 'f1:', f1, 'kappa:', kappa)
+    print('[SVM] training: ', 'f1:', f1, 'kappa:', kappa, "done in %0.3fs." % (time() - t0))
     with open(batch + '_svm_clf.pickle', 'wb') as f:
         pickle.dump(clf, f)
     with open(batch + '_svm_searcher.pickle',
@@ -183,6 +185,7 @@ def svm_model(xtrain, xtest, ytrain, ytest, batch):
 
 # %%
 def lg_model(xtrain, xtest, ytrain, ytest, batch):
+    t0 = time()
     param_grid = {'penalty': ['l1', 'l2', 'elasticnet'],
                   'max_iter': range(10, 50, 10)}
     clf = GridSearchCV(
@@ -191,7 +194,7 @@ def lg_model(xtrain, xtest, ytrain, ytest, batch):
     searcher = clf.fit(xtrain, ytrain)
     estimator = searcher.best_estimator_
     f1, kappa = measures(estimator.predict(xtest), ytest)
-    print('[LG] training: ', 'f1:', f1, 'kappa:', kappa)
+    print('[LG] training: ', 'f1:', f1, 'kappa:', kappa, "done in %0.3fs." % (time() - t0))
     with open(batch + '_lg_clf.pickle', 'wb') as f:
         pickle.dump(clf, f)
     with open(batch + '_lg_searcher.pickle',
@@ -207,6 +210,7 @@ def lg_model(xtrain, xtest, ytrain, ytest, batch):
 
 # %%
 def dt_model(xtrain, xtest, ytrain, ytest, batch):
+    t0 = time()
     param_grid = {'min_samples_split': range(2, 403, 20)}
     clf = GridSearchCV(
         tree.DecisionTreeClassifier(), param_grid, scoring=scoring
@@ -214,7 +218,7 @@ def dt_model(xtrain, xtest, ytrain, ytest, batch):
     searcher = clf.fit(xtrain, ytrain)
     estimator = searcher.best_estimator_
     f1, kappa = measures(estimator.predict(xtest), ytest)
-    print('[dt] training: ', 'f1:', f1, 'kappa:', kappa)
+    print('[dt] training: ', 'f1:', f1, 'kappa:', kappa, "done in %0.3fs." % (time() - t0))
     with open(batch + '_dt_clf.pickle', 'wb') as f:
         pickle.dump(clf, f)
     with open(batch + '_dt_searcher.pickle',
@@ -230,12 +234,13 @@ def dt_model(xtrain, xtest, ytrain, ytest, batch):
 
 # %%
 def nb_model(xtrain, xtest, ytrain, ytest, batch):
+    t0 = time()
     param_grid = {}
     clf = GridSearchCV(GaussianNB(), param_grid)
     searcher = clf.fit(xtrain, ytrain)
     estimator = searcher.best_estimator_
     f1, kappa = measures(estimator.predict(xtest), ytest)
-    print('[NB] training: ', 'f1:', f1, 'kappa:', kappa)
+    print('[NB] training: ', 'f1:', f1, 'kappa:', kappa, "done in %0.3fs." % (time() - t0))
     with open(batch + '_nb_clf.pickle', 'wb') as f:
         pickle.dump(clf, f)
     with open(batch + '_nb_searcher.pickle',
@@ -245,6 +250,33 @@ def nb_model(xtrain, xtest, ytrain, ytest, batch):
       'wb') as sfm:
         pickle.dump(estimator, sfm)
     return searcher, estimator, clf, f1, kappa
+
+def nn_model(xtrain, xtest, ytrain, ytest, batch):
+    t0 = time()
+    hidden_layer_sizes = (10, 50, 50, 10)
+    param_grid = {
+                  'alpha': [1e-5, 1e-4, 1e-3],
+                  'solver': ['lbfgs', 'sgd', 'adam'],
+                  'activation': ['identity', 'logistic', 'tanh', 'relu'],
+                 }
+    if batch == 'lyrics':
+        hidden_layer_sizes=(100, 200, 100, 50)
+    clf = GridSearchCV(MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, random_state=905, early_stopping=True), param_grid)
+    searcher = clf.fit(xtrain, ytrain)
+    estimator = searcher.best_estimator_
+    f1, kappa = measures(estimator.predict(xtest), ytest)
+    print('[nn] training: ', 'f1:', f1, 'kappa:', kappa, "done in %0.3fs." % (time() - t0))
+    with open(batch + '_nn_clf.pickle', 'wb') as f:
+        pickle.dump(clf, f)
+    with open(batch + '_nn_searcher.pickle',
+              'wb') as sf:
+        pickle.dump(searcher, sf)
+    with open(batch + '_nn_estimator.pickle',
+      'wb') as sfm:
+        pickle.dump(estimator, sfm)
+    return searcher, estimator, clf, f1, kappa
+
+
 
 # %% [markdown]
 # ## Data spliting
@@ -288,6 +320,7 @@ print(xtrain.shape, xtest.shape, ytrain.shape, ytest.shape)
 
 
 # %%
+nn_searcher, nn_estimator, nn_clf, nn_f1, nn_kappa = nn_model(xtrain, xtest, ytrain, ytest, batch='Combined')
 svm_searcher, svm_estimator, svm_clf, svm_f1, svm_kappa = svm_model(xtrain, xtest, ytrain, ytest, batch='Combined')
 lg_searcher, lg_estimator, lg_clf, lg_f1, lg_kappa = lg_model(xtrain, xtest, ytrain, ytest, batch='Combined')
 dt_searcher, dt_estimator, dt_clf, dt_f1, dt_kappa = dt_model(xtrain, xtest, ytrain, ytest, batch='Combined')
@@ -305,10 +338,11 @@ xtrain_.shape, xtest_.shape, ytrain_.shape, ytest_.shape
 
 
 # %%
-svm_searcher, svm_estimator, svm_clf, svm_f1, svm_kappa = svm_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
-lg_searcher, lg_estimator, lg_clf, lg_f1, lg_kappa = lg_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
-dt_searcher, dt_estimator, dt_clf, dt_f1, dt_kappa = dt_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
-nb_searcher, nb_estimator, nb_clf, nb_f1, nb_kappa = nb_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
+nn_searcher_, nn_estimator_, nn_clf_, nn_f1_, nn_kappa_ = nn_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
+svm_searcher_, svm_estimator_, svm_clf_, svm_f1_, svm_kappa_ = svm_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
+lg_searcher_, lg_estimator_, lg_clf_, lg_f1_, lg_kappa_ = lg_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
+dt_searcher_, dt_estimator_, dt_clf_, dt_f1_, dt_kappa_ = dt_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
+nb_searcher_, nb_estimator_, nb_clf_, nb_f1_, nb_kappa_ = nb_model(xtrain_, xtest_, ytrain_, ytest_, batch='lyrics')
 
 
 # %%
